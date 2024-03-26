505f6129db015e039d4cc05ec63d075caca508f9
taladromaryann022 <143863370+taladromaryann022@users.noreply.github.com>
2024-03-26 09:04:28 +0800

    03-26-24

--- models.js
+++ models.js
@@ -2,6 +2,7 @@ const { Sequelize, DataTypes } = require('sequelize');
 const sequelize = new Sequelize('Cooperativedb', 'postgres', 'Ctugk3nd3s', {  
     host: 'localhost',
     dialect: 'postgres',
+    port: 5432,
 });
 
 
@@ -102,66 +103,339 @@ Savings.belongsTo(User, {
     onDelete: 'CASCADE' 
 });
 
-(async () => {
-    try {
-        await sequelize.sync({ force: true });
-        console.log("Tables synced successfully");
-    } catch (error) {
-        console.error("Error syncing tables:", error);
-    } finally {
-        sequelize.close();
-    }
-})();
+// // 
 
-const CBU = sequelize.define('CBU', {  
-    user_id: {    
-        type: DataTypes.INTEGER,     
-        primaryKey: true,     
-        autoIncrement: true  
-    },   
-    cbu_id: {     
-        type: DataTypes.INTEGER,     
-        allowNull: false 
-    },   
-    amount: {     
-        type: DataTypes.FLOAT,     
-        allowNull: false 
-    },   
-    interest: {     
-        type: DataTypes.FLOAT,     
-        allowNull: false  
-    },  
-    loan_id: {     
-        type: DataTypes.INTEGER  
-    },   
-    timestamp: {     
-        type: DataTypes.DATE,     
-        defaultValue: Sequelize.NOW  
-    } 
+
+const CBU = sequelize.define('CBU', {
+    user_id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+    },
+    cbu_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    },
+    amount: {
+    type: DataTypes.FLOAT,
+    allowNull: false,
+    },
+    interest: {
+    type: DataTypes.FLOAT,
+    allowNull: false,
+    },
+    loan_id: {
+    type: DataTypes.INTEGER,
+    },
+    timestamp: {
+    type: DataTypes.DATE,
+    defaultValue: Sequelize.NOW,
+    },
 });
 
 CBU.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE',
+});
+
+// (async () => {
+//     try {
+//         const schema = await sequelize.getQueryInterface().describeTable('CBU');
+//         console.log(schema);
+//     } catch (error) {
+//         console.error("Error getting table schema:", error);
+//     } finally {
+//         await sequelize.close();
+//     }
+// })();
+
+const Loan = sequelize.define('Loan', {
+    user_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    },
+    application_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    },
+    loan_id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+    },
+    loan_status: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    },
+    payment: {
+    type: DataTypes.FLOAT,
+    allowNull: false,
+    },
+    balance: {
+    type: DataTypes.FLOAT,
+    allowNull: false,
+    },
+    savings: {
+    type: DataTypes.FLOAT,
+    allowNull: false,
+    },
+    loan_term: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    },
+    timestamp: {
+    type: DataTypes.DATE,
+    defaultValue: Sequelize.NOW,
+    },
+});
+
+
+Loan.belongsTo(User, {
     foreignKey: 'user_id', 
     onDelete: 'CASCADE' 
 });
 
+// (async () => {
+//     try {
+//         const schema = await sequelize.getQueryInterface().describeTable('Loan');
+//         console.log(schema);
+//     } catch (error) {
+//         console.error("Error getting table schema:", error);
+//     } finally {
+//         await sequelize.close();
+//     }
+// })();
 
-(async () => {
-    try {
-        await sequelize.sync({ force: true });
-        console.log("Tables synced successfully");
-    } catch (error) {
-        console.error("Error syncing tables:", error);
-    } finally {
-        sequelize.close();
-    }
-})();
+const Loan_application = sequelize.define('Loan_application', {
+    user_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    },
+    application_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    },
+    application_status: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    },
+    loan_type: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    },
+    amount: {
+    type: DataTypes.FLOAT,
+    allowNull: false,
+    },
+    loan_term: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    },
+    interest: {
+    type: DataTypes.FLOAT,
+    allowNull: false,
+    },
+    timestamp: {
+    type: DataTypes.DATE,
+    defaultValue: Sequelize.NOW,
+    },
+});
 
+Loan_application.belongsTo(User, {
+    foreignKey: 'user_id', 
+    onDelete: 'CASCADE' 
+});
+
+// (async () => {
+//     try {
+//         const schema = await sequelize.getQueryInterface().describeTable('Loan_application');
+//         console.log(schema);
+//     } catch (error) {
+//         console.error("Error getting table schema:", error);
+//     } finally {
+//         await sequelize.close();
+//     }
+// })();
+
+const Transaction = sequelize.define('Transaction', {
+    user_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    },
+    savings_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    },
+    transaction_id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+    },
+    transaction_type: {
+    type: DataTypes.ENUM('deposit', 'withdraw'),
+    allowNull: false,
+    },
+    amount: {
+    type: DataTypes.FLOAT,
+    allowNull: false,
+    },
+    timestamp: {
+    type: DataTypes.DATE,
+    defaultValue: Sequelize.NOW,
+    },
+});
+
+Transaction.belongsTo(User, {
+    foreignKey: 'user_id', 
+    onDelete: 'CASCADE' 
+});
+
+// (async () => {
+//     try {
+//         const schema = await sequelize.getQueryInterface().describeTable('Transaction');
+//         console.log(schema);
+//     } catch (error) {
+//         console.error("Error getting table schema:", error);
+//     } finally {
+//         await sequelize.close();
+//     }
+// })();
+
+const CBUTransaction = sequelize.define('CBUTransaction', {
+    user_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    },
+    cbu_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    },
+    cbu_transaction_id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+    },
+    transaction_type: {
+    type: DataTypes.ENUM('deposit', 'withdraw'),
+    allowNull: false,
+    },
+    amount: {
+    type: DataTypes.FLOAT,
+    allowNull: false,
+    },
+    timestamp: {
+    type: DataTypes.DATE,
+    defaultValue: Sequelize.NOW,
+    },
+});
+
+CBUTransaction.belongsTo(User, {
+    foreignKey: 'user_id', 
+    onDelete: 'CASCADE' 
+});
+
+// (async () => {
+//     try {
+//         const schema = await sequelize.getQueryInterface().describeTable('CBUTransaction');
+//         console.log(schema);
+//     } catch (error) {
+//         console.error("Error getting table schema:", error);
+//     } finally {
+//         await sequelize.close();
+//     }
+// })();
+
+const History = sequelize.define('History', {
+    user_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    },
+    application_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    },
+    transaction_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+    },
+    cbu_transaction_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    },
+    loan_id: {
+    type: DataTypes.INTEGER,
+    },
+    history_id: {
+    type: DataTypes.INTEGER,
+    },
+    timestamp: {
+    type: DataTypes.DATE,
+    defaultValue: Sequelize.NOW,
+    allowNull: false,
+    },
+});
+
+// (async () => {
+//     try {
+//         const schema = await sequelize.getQueryInterface().describeTable('History');
+//         console.log(schema);
+//     } catch (error) {
+//         console.error("Error getting table schema:", error);
+//     } finally {
+//         await sequelize.close();
+//     }
+// })();
+
+const Chat = sequelize.define('Chat', {
+    user_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    },
+    chat_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+    },
+    recipient: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    },
+    chat: {
+    type: DataTypes.TEXT,
+    allowNull: false,
+    },
+    timestamp: {
+    type: DataTypes.DATE,
+    defaultValue: Sequelize.NOW,
+    allowNull: false,
+    },
+});
+
+// (async () => {
+//     try {
+//         const schema = await sequelize.getQueryInterface().describeTable('History');
+//         console.log(schema);
+//     } catch (error) {
+//         console.error("Error getting table schema:", error);
+//     } finally {
+//         await sequelize.close();
+//     }
+// })();
 
                 
-// Application.sync();
-// User.sync();
-//Savings.sync();
+Application.sync();
+User.sync();
+Savings.sync();
+CBU.sync();
+Loan.sync();                              
+Loan_application.sync();
+Transaction.sync();
+CBUTransaction.sync();
+History.sync();
+Chat.sync();
 
 // Application.sync()
 //   .then(() => {
@@ -178,29 +452,18 @@ CBU.belongsTo(User, {
 //   .catch(error => {
 //     console.error('Error syncing User model:', error);
 //   });
-
-// Savings.sync()
-//   .then(() => {
-//     console.log('Savings model synced successfully');
-//   })
-//   .catch(error => {
-//     console.error('Error syncing Savings model:', error);
-//   });
-
-// CBU.sync()
-//   .then(() => {
-//     console.log('CBU model synced successfully');
-//   })
-//   .catch(error => {
-//     console.error('Error syncing CBU model:', error);
-//   });
-
-
 module.exports = {
     Application,
     User,
     Savings,
     CBU,
+    Loan,
+    Loan_application,
+    Transaction,
+    CBUTransaction,
+    History,
+    Chat,
+    
 }
 
 
