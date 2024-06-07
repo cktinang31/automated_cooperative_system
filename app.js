3cff6ea6d28aaf36f977b3bfc3b29b517161d843
kaneki-khen <kanekikhen677@gmail.com>
2024-06-07 15:28:23 +0900

    ken-6-7-24

--- app.js
+++ app.js
@@ -322,6 +322,6 @@ app.use((req, res) => {
   res.status(404).render('404', { title: '404'})
 });
 
-app.listen(3000, () => {
+app.listen(8080, () => {
   console.log('Server running on port 3000');
 });
