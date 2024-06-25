cff58f5eab2d3714b0f2b1575f193c9c266a09b8
cktinang31 <158656484+cktinang31@users.noreply.github.com>
2024-06-25 12:41:44 +0800

    kristine/06-25-24

--- routes/memberpageRoute.js
+++ routes/memberpageRoute.js
@@ -370,5 +370,26 @@ router.get('/Member/sidebar', async (req, res, next) => {
 router.get('/Manager/membersinfo', (req, res) => {
     res.render('Manager/memberinfo', { title: 'Member'});
   });
+
+router.get('/Member/dashboard', (req,res, next) =>{
+try {
+    console.log('Session ID:', req.sessionID);
+    console.log('Session:', req.session);
+    console.log('Authenticated:', req.isAuthenticated());
+
+    if (req.isAuthenticated() && req.user && req.user.role === 'regular') {
+        console.log('User is authenticated a regular.');
+        const user = req.user;
+        res.render('./Member/dashboard', { title: 'Dashboard', user });
+    } else {
+        console.log('User is not authenticated. Redirecting to login page.');
+        req.session.returnTo = req.originalUrl;
+        res.redirect('/login');
+    }
+} catch (error) {
+    console.error('Error in isAuthenticated middleware:', error);
+    res.status(500).send('Internal server error');
+}    
+});
   
 module.exports = router;
