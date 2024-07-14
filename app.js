933af657340413a5f345a2e5d6c3067c33272230
kaneki-khen <144084528+kaneki-khen@users.noreply.github.com>
2024-07-14 07:13:58 -0400

    071524

--- app.js
+++ app.js
@@ -110,6 +110,7 @@ app.use(express.static('public'));
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 app.use((req, res, next) => {
   console.log('Request body:', req.body);
   next();
