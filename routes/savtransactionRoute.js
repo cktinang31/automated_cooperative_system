35f897c82e399e9e1739ca05cfaf11bc3d468150
cktinang31 <158656484+cktinang31@users.noreply.github.com>
2024-12-04 08:15:18 +0800

    kristine12/04/24

--- routes/savtransactionRoute.js
+++ routes/savtransactionRoute.js
@@ -5,5 +5,7 @@ const router = express.Router();
 
 router.post('/savings_transaction', savtransactionController.savings_transaction);
 router.post('/update_savings_request/:id', savtransactionController.update_savings_request);
+router.post('/handle_transaction_submission', savtransactionController.handle_transaction_submission);
+
 
 module.exports = router;
